7972b0f961f43ce9553ce44c3a821d1924bb2e5a
Saud Masood <saudabbasi030650@gmail.com>
2024-08-12 11:58:21 +0500

    Create readme.r

---
+++ readme.r
@@ -0,0 +1,5 @@
+I'm a passionate  Developer Enthusiast currently pursuing my Bachelor's in Computer Science at NSU, Islamabad. Welcome to my GitHub profile!
+
+🚀 Currently expanding my Programming Skills.
+💻 Exploring Software Engineering and Data Structures.
+🌟 Open to collaborations and new opportunities.
